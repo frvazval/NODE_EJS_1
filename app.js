38c4af8cd281994fd29847d314426abbc8dfc19f
Fvazquez <foapgenerico@365.stucom.com>
2025-06-18 19:26:14 +0200

    stucon

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ const app = express();
 process.loadEnvFile();
 const PORT = process.env.PORT;
 
-// para que encuentre los recursos en la carpeta public
+// para que encuentre los recursos en la carpeta public, css, js, img, etc...
 app.use(express.static(path.join(__dirname, "public")));
+
+// Informar a express de cual es el motor de las plantillas
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
     // res.send("Todo OK")
     res.send(__dirname);
+})
+
+app.get("/alumnos", (req, res) => {
+    res.render("alumnos", {"h1": "Título enviado desde la ruta", "titulo": "Alumnos EJS"});
+})
+
+app.get("/cursos", (req, res) => {
+    res.render("cursos", {"h1": "Cursos disponibles", "titulo": "Cursos EJS"});
 })
 
 app.use((req, res) => {
